852df3a85a9aff0cec782b17469f39bdec5cb9e2
Prashant Singh <mprashantks@gmail.com>
2017-11-10 01:43:46 +0530

    Delete command mongo

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -5,9 +5,13 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
     return console.log('Unable to connect to MongoDB database: '+error);
   console.log('Connected to MongoDB database');
 
-  db.collection('Users').find({name: 'prashant'}).count().then((count) => {
-    console.log(`Users with name prashant: ${count}`);
+  db.collection('Users').findOneAndDelete({name: 'prashant'}).then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
   });
 
-  db.close();
+  // db.collection('Users').find({name: 'prashant'}).count().then((count) => {
+  //   console.log(`Users with name prashant: ${count}`);
+  // });
+  //
+  // db.close();
 });
