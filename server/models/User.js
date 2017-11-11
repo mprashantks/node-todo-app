99842e4dd84584a6fed35be7d79c0dd40fd599f6
Prashant Singh <mprashantks@gmail.com>
2017-11-12 01:02:04 +0530

    Add POST /users/login route

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -64,6 +65,41 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.access': 'auth',
     'tokens.token': token
+  });
+};
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt,  (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    });
+  } else {
+    next();
+  }
+});
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+          if(res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+      })
+    });
   });
 };
 
